28cc05990b3214e3ddaa5b25f5737f9c0359da00
KK <KK@test.com>
2021-07-07 01:32:23 +0800

    learn @Valid

--- src/main/java/com/example/selflearning/validator/controller/DepartmentController.java
+++ src/main/java/com/example/selflearning/validator/controller/DepartmentController.java
@@ -1,6 +1,5 @@
 package com.example.selflearning.validator.controller;
 
-import com.alibaba.fastjson.JSON;
 import com.example.selflearning.validator.entity.Department;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
