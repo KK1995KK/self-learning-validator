8f315e3e3db5bf050d0654e952beeb580674e4a1
KK <KK@test.com>
2021-07-07 00:03:07 +0800

    init

---
+++ src/main/java/com/example/selflearningvalidator/SelfLearningValidatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.selflearningvalidator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfLearningValidatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SelfLearningValidatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/selflearningvalidator/SelfLearningValidatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.selflearningvalidator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelfLearningValidatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
